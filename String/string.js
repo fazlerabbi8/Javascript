9e2fb3080a6db33f1970a8b670e7b8dcebbc1c2c
fazlerabbi13 <mohammad0132794@gmail.com>
2026-03-22 20:07:45 +0600

    check some portion of string exist or not in the main string.

--- String/string.js
+++ String/string.js
@@ -3,9 +3,14 @@ let str = "Hello World!";
 
 console.log(str.length);
 
-a = str.slice(0,4);
-b = str.slice(5,8);
+// a = str.slice(0,4);
+// b = str.slice(5,8);
 
 // console.log(a);
 // console.log(b);
+
+let result = str.includes("llo");
+// let result2 = str.includes("l", 10);
+console.log(result);
+// console.log(result2);
 
